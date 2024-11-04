10b4fbef8f6c4abd7901c0cae1515e38a65829cd
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2024-11-04 04:19:19 +0000

    Revert a couple lines.

--- .devcontainer/fdp-setup.sh
+++ .devcontainer/fdp-setup.sh
@@ -3,8 +3,7 @@
 echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.bashrc
 pip3 install --user -r requirements.txt
 
-# Not necessary for this project.
-# /usr/local/flutter/bin/flutter create .
-# django-admin startproject $(basename "$PWD") .
+/usr/local/flutter/bin/flutter create .
+django-admin startproject $(basename "$PWD") .
 
 clear && echo "devcontainer configuration complete."
