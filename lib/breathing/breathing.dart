992b6ce0d38f127a46798e439d8b5341d2b2ed86
Vrome-1 <vaanirometra@yahoo.com>
2024-11-18 00:46:19 -0600

    Update breathing.dart

--- lib/breathing/breathing.dart
+++ lib/breathing/breathing.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Breathing'),
+      ),
+      body: Center(
+        child: Text('Let's work on breathing. As the circle expands take a deep breath in and as the circle retracts let the air out. This can help with calming down.'),
+        child: Container(
+            width: 100.0,
+            height: 100.0, 
+            decoration: BoxDecoration(
+              color: Colors.yellow, 
+              shape: BoxShape.circle,
+      ),
+    );
+  }
+}
