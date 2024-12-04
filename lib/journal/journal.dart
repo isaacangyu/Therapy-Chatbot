601a327ea83fbac14480022f4bca496bc00801c0
Shrida Bhat <68925009+shrida-b@users.noreply.github.com>
2024-12-04 08:02:00 +0000

    Updated journal.dart, need to add rest of files

--- lib/journal/journal.dart
+++ lib/journal/journal.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'journal_screen.dart';
 
+class JournalPage extends StatelessWidget {
+  const JournalPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Journal Page',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 45, 221, 98)),
+        useMaterial3: true,
+      ),
+      home: JournalScreen(),
+    );
+  }
+}
