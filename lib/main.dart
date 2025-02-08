46185246c9e5713f64eef7dc951520b7a12b77af
isaacangyu <isaacangyu@gmail.com>
2025-02-08 22:11:46 +0000

    Temporary commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 45, 221, 98)),
+            seedColor: const Color.fromARGB(255, 77, 24, 224)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Mental Health Chatbot Home Page'),
