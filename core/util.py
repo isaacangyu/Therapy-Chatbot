36122523edcfc54788e660c66d42c33bd2b0ede9
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2025-03-07 03:36:34 +0000

    Offload data decryption to wrapper. Restore CSRF protection and disable it via wrapper too.

--- core/util.py
+++ core/util.py
@@ -1,8 +1,9 @@
 import json
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-from core import crypto
+from core import crypto, util
 
 def cors_signed_json_response(data):
     response_data = json.dumps(data)
@@ -11,3 +12,12 @@ def cors_signed_json_response(data):
     response = HttpResponse(response_data)
     response["Access-Control-Allow-Origin"] = "*"
     return response
+
+def app_view(view):
+    @csrf_exempt
+    def wrapper(request):
+        raw_data = crypto.asymmetric_decrypt(request.body)
+        form = json.loads(raw_data)
+        response = view(request, form)
+        return util.cors_signed_json_response(response)
+    return wrapper
