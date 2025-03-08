1a8976b44813402874ad7d872b8e18662e2c62b1
isaacangyu <isaacangyu@gmail.com>
2025-03-08 05:35:59 +0000

    created chatbot app and basic database

--- lib/breathing/breathing.dart
+++ lib/breathing/breathing.dart
@@ -10,16 +10,15 @@ class BreathingPage extends StatelessWidget {
         title: const Text('Breathing'),
       ),
       body: Center(
-        child: Container(
-            width: 100.0,
-            height: 100.0, 
-            decoration: const BoxDecoration(
-              color: Colors.yellow, 
-              shape: BoxShape.circle,
-            ),
-            child: const Text("Let's work on breathing. As the circle expands take a deep breath in and as the circle retracts let the air out. This can help with calming down.")
-        )
-      ),
+          child: Container(
+              width: 100.0,
+              height: 100.0,
+              decoration: const BoxDecoration(
+                color: Colors.yellow,
+                shape: BoxShape.circle,
+              ),
+              child: const Text(
+                  "Let's work on breathing. As the circle expands take a deep breath in and as the circle retracts let the air out. This can help with calming down."))),
     );
   }
 }
--- therapy_chatbot/settings.py
+++ therapy_chatbot/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core.apps.CoreConfig",
+    "chatbot.apps.ChatbotConfig",
 ]
 
 MIDDLEWARE = [
