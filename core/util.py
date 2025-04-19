9b975ac9345d042798c9efd2b8cb8b9142865462
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2025-04-19 16:26:08 +0000

    Fix HTTP OPTIONS preflight request issue.

--- core/util.py
+++ core/util.py
@@ -1,8 +1,9 @@
-import json
+import json, functools
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_POST
+from django.conf import settings
 from core import crypto, util
 
 def cors_signed_json_response(data):
@@ -10,14 +11,27 @@ def cors_signed_json_response(data):
     response_data = crypto.asymmetric_sign(response_data.encode())
     
     response = HttpResponse(response_data)
-    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Origin"] = settings.ACCESS_CONTROL_ALLOW_ORIGIN
     return response
 
 def app_view(view):
+    @functools.wraps(view)
     @csrf_exempt
-    def wrapper(request):
+    def wrapper(request, *args, **kwargs):
         raw_data = crypto.asymmetric_decrypt(request.body)
         form = json.loads(raw_data)
-        response = view(request, form)
+        response = view(request, form, *args, **kwargs)
         return util.cors_signed_json_response(response)
+    return wrapper
+
+def require_POST_OPTIONS(view):
+    @functools.wraps(view)
+    def wrapper(request, *args, **kwargs):
+        if request.method == "OPTIONS":
+            response = HttpResponse()
+            response["Access-Control-Allow-Origin"] = settings.ACCESS_CONTROL_ALLOW_ORIGIN
+            response["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+            # response["Access-Control-Allow-Headers"] = "Cache-Control"
+            return response
+        return require_POST(view)(request, *args, **kwargs)
     return wrapper
