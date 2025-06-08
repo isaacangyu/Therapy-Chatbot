0821639ad4429f3b9730ab5a20131a0e01f0af57
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2025-06-08 20:32:20 +0000

    Upgrade Flutter to 3.29.0 (Dart 3.7.0). Begin migration to alternative chat UI library.

--- lib/chatbot/chatbot.dart
+++ lib/chatbot/chatbot.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:provider/provider.dart';
 
-import '/chatbot/chatbot_provider.dart';
 import '/util/theme.dart';
 import '/util/network.dart';
 import '/util/global.dart';
@@ -27,63 +24,7 @@ class ChatbotPage extends StatelessWidget implements SwitchActions {
         ), 
         backgroundColor: theme.colorScheme.primary
       ),
-      body: appState.session.online ? LlmChatView(
-        provider: ChatbotProvider(),
-        welcomeMessage: 'Welcome!',
-        style: LlmChatViewStyle(
-          backgroundColor: customTheme.primaryColor,
-          progressIndicatorColor: customTheme.activeColor,
-          userMessageStyle: UserMessageStyle(
-            textStyle: theme.textTheme.bodyMedium!.copyWith(
-              color: theme.colorScheme.onSecondaryFixed,
-            ),
-            decoration: (UserMessageStyle.defaultStyle().decoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryFixedDim,
-            ),
-          ),
-          llmMessageStyle: LlmMessageStyle(
-            icon: Icons.face_6,
-            iconColor: theme.colorScheme.onSecondaryContainer,
-            iconDecoration: (LlmMessageStyle.defaultStyle().iconDecoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryContainer,
-            ),
-            decoration: (LlmMessageStyle.defaultStyle().decoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryContainer,
-              border: Border.all(color: customTheme.inactiveColor),
-            ),
-            // We may have to update this in the future if using additional markdown syntax.
-            markdownStyle: MarkdownStyleSheet(
-              p: theme.textTheme.bodyMedium!.copyWith(
-                color: theme.colorScheme.onSecondaryContainer,
-              ),
-            ),
-          ),
-          chatInputStyle: ChatInputStyle(
-            textStyle: theme.textTheme.bodyMedium!.copyWith(
-              color: theme.colorScheme.onSecondaryContainer,
-            ),
-            hintStyle: theme.textTheme.bodyMedium!.copyWith(
-              color: theme.colorScheme.surface,
-            ),
-            hintText: 'Type a message...',
-            backgroundColor: customTheme.primaryColor,
-            decoration: (ChatInputStyle.defaultStyle().decoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryContainer,
-              border: Border.all(color: customTheme.inactiveColor),
-            ),
-          ),
-          addButtonStyle: ActionButtonStyle(
-            iconDecoration: (ActionButtonStyle.defaultStyle(ActionButtonType.add).iconDecoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryContainer,
-            ),
-          ),
-          recordButtonStyle: ActionButtonStyle(
-            iconDecoration: (ActionButtonStyle.defaultStyle(ActionButtonType.add).iconDecoration as BoxDecoration).copyWith(
-              color: theme.colorScheme.secondaryContainer,
-            ),
-          ),
-        ),
-      ) : const Center(child: Text('Chatbot not available offline ☹️.')),
+      body: appState.session.online ? const Placeholder() : const Center(child: Text('Chatbot not available offline ☹️.')),
     );
   }
   
@@ -102,8 +43,8 @@ class ChatbotPage extends StatelessWidget implements SwitchActions {
       }
     ));
     if (status.ok) {
-      channel = status.channel;
-      broadcast = status.broadcast;
+      // channel = status.channel;
+      // broadcast = status.broadcast;
     } else {
       if (!context.mounted) {
         return;
@@ -118,9 +59,9 @@ class ChatbotPage extends StatelessWidget implements SwitchActions {
   
   @override
   void onExitFocus(BuildContext context) async {
-    if (channel != null) {
-      await websocketClose(channel!);
-    }
-    channel = null;
+    // if (channel != null) {
+    //   await websocketClose(channel!);
+    // }
+    // channel = null;
   }
 }
