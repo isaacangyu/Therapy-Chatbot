a0b1759cda5eb5a5703e699f0103eb72e40cd6f6
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2025-09-21 00:57:37 +0000

    Partially successful Android emulation.

--- .devcontainer/fdp-setup.sh
+++ .devcontainer/fdp-setup.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.bashrc
+setup() {
+    (
+        set -e
 
-pipx install poetry && poetry install
+        echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.bashrc
 
-clear && echo "devcontainer configuration complete."
+        export ANDROID_HOME=/usr/local/android_sdk
+        echo 'export ANDROID_HOME='$ANDROID_HOME >> ~/.bashrc
+        echo 'export PATH=$PATH:'$ANDROID_HOME'/cmdline-tools/latest:'$ANDROID_HOME'/cmdline-tools/latest/bin:'$ANDROID_HOME'/platform-tools' >> ~/.bashrc
+
+        pipx install poetry && poetry install
+
+        # Android Development Packages
+        # Android Version: 15 (Vanilla Ice Cream)
+        # API Level: 35
+        yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platforms;android-35" "build-tools;35.0.0" "platform-tools"
+        yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --licenses
+    )
+    return $?
+}
+
+setup
+
+STATUS=$?
+if [[ $STATUS -ne 0 ]]; then
+    echo -e '\e[91;1mDev Container setup completed with errors. See the log above for details.\e[m'
+    exit 1
+else
+    echo -e '\e[92;1mDev Container setup complete.\e[m'
+fi
